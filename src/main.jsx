02ab4a9e4058660c43e5b8a14faeae40f7c14e68
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-11-24 17:56:39 +0300

    Refactor main.jsx to implement routing with LandingPage and LoginPage components

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,23 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthListener from "./App";
+import LandingPage from "@/pages/LandingPage";
+import LoginPage from "@/pages/Login/page";
+import "./index.css"; // if you have one
 
-createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthListener>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          {/* add other routes here */}
+        </Routes>
+      </AuthListener>
+    </BrowserRouter>
+  </React.StrictMode>
+);
