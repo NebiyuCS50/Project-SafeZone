e624f34c5d7292f185bff719bb59e667545d2301
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-12-01 10:25:28 +0300

    Update signup schema to include phone number validation and optional photo URL

--- src/validation/signupSchema.js
+++ src/validation/signupSchema.js
@@ -2,9 +2,29 @@ import { z } from "zod";
 
 export const SignupSchema = z
   .object({
-    email: z.string().email("Enter a valid email"),
+    name: z.string().min(2, "Name must be at least 2 characters"),
+    phoneNumber: z.string().refine(
+      (val) => {
+        if (!val) return false;
+
+        if (val.startsWith("+251")) {
+          return /^\+251\d{8}$/.test(val);
+        }
+
+        if (val.startsWith("09")) {
+          return /^09\d{8}$/.test(val);
+        }
+        return false;
+      },
+      {
+        message:
+          "Phone must start with +251 (12 chars total) or 09 (10 chars total)",
+      }
+    ),
+    email: z.email("Enter a valid email"),
     password: z.string().min(8, "Password must be at least 8 characters"),
     confirm: z.string().min(8, "Confirm password must match"),
+    photoUrl: z.url("Enter a valid image URL").optional(),
   })
   .refine((data) => data.password === data.confirm, {
     message: "Passwords do not match",
