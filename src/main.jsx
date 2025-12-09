1c5f981ca6c9993b3c8e58b2fe750d7304980316
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-12-09 15:34:55 +0300

    Add Toaster component for improved notification handling

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes/routes.jsx";
 import AuthListener from "./App"; // your auth listener
 import "./index.css";
+import { Toaster } from "sonner";
 
 const router = createBrowserRouter(routes);
 const root = createRoot(document.getElementById("root"));
@@ -11,6 +12,7 @@ const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <AuthListener>
+      <Toaster position="top-right" richColors />
       <RouterProvider router={router} />
     </AuthListener>
   </StrictMode>
