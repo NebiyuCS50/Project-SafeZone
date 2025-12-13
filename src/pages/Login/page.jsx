74ed42e61dd024630e6e9779cb70a4806217b6cc
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-12-13 17:08:41 +0300

    feat: replace anchor tag with Link component for navigation in LoginPage

--- src/pages/Login/page.jsx
+++ src/pages/Login/page.jsx
@@ -2,19 +2,19 @@ import { GalleryVerticalEnd } from "lucide-react";
 import logo from "@/assets/Logo.png";
 import SafeZone from "@/assets/SafeZone.jpeg";
 import { LoginForm } from "@/components/login-form";
+import { Link } from "react-router-dom";
 
 export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
+          <Link to="/" className="flex items-center gap-1 font-medium">
             <div className="bg-white text-primary-foreground flex size-13 items-center justify-center rounded-md">
               <img src={logo} alt="Logo" />
             </div>
-
-            <p className="text-lg font-semibold mr-8">Safe Zone</p>
-          </a>
+            SafeRoute
+          </Link>
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
